877c379ada6db6d0606fd9c4c9ef4ad28b0a1168
harinderlabana <sunny.labana@gmail.com>
2022-09-21 19:53:57 -0400

    created the Confirm child for Appointment

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
 
 storiesOf('Button', module)
   .addParameters({
@@ -155,5 +156,17 @@ storiesOf('Show', module)
       interviewer={interviewer}
       onEdit={action('onEdit')}
       onDelete={action('onDelete')}
+    />
+  ));
+
+storiesOf('Confirm', module)
+  .addParameters({
+    backgrounds: [{name: 'white', value: '#fff', default: true}],
+  })
+  .add('Confirm', () => (
+    <Confirm
+      message='Delete the appointment'
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
     />
   ));
