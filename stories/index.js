da32c9fd83a82aaf49e1abc405cb25bd24f84e09
harinderlabana <sunny.labana@gmail.com>
2022-09-21 19:46:51 -0400

    implemented the Show component in Appointment

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
 
 storiesOf('Button', module)
   .addParameters({
@@ -143,3 +144,16 @@ storiesOf('Empty', module)
     backgrounds: [{name: 'white', value: '#fff', default: true}],
   })
   .add('Empty', () => <Empty onAdd={action('onAdd')} />);
+
+storiesOf('Show', module)
+  .addParameters({
+    backgrounds: [{name: 'white', value: '#fff', default: true}],
+  })
+  .add('Show', () => (
+    <Show
+      student='Lydia Miller-Jones'
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ));
