a97ead3e219966af1471a861e4b6887d2723fae4
harinderlabana <sunny.labana@gmail.com>
2022-09-21 19:59:13 -0400

    created the Status child for Appointment

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
 
 storiesOf('Button', module)
   .addParameters({
@@ -170,3 +171,9 @@ storiesOf('Confirm', module)
       onCancel={action('onCancel')}
     />
   ));
+
+storiesOf('Status', module)
+  .addParameters({
+    backgrounds: [{name: 'white', value: '#fff', default: true}],
+  })
+  .add('Status', () => <Status message='Deleting' />);
